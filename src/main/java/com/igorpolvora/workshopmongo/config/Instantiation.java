3e137e9c446e05de561ba3e6ae1fb99df6387cb2
Igor Polvora <igorpolvora@gmail.com>
2026-05-29 16:55:15 -0300

    Creating Entity Post with Nested User

--- src/main/java/com/igorpolvora/workshopmongo/config/Instantiation.java
+++ src/main/java/com/igorpolvora/workshopmongo/config/Instantiation.java
@@ -5,6 +5,11 @@ import com.igorpolvora.workshopmongo.domain.User;
 import com.igorpolvora.workshopmongo.repository.UserRepository;
 import java.util.Arrays;
 import org.springframework.context.annotation.Configuration;
+import com.igorpolvora.workshopmongo.domain.Post;
+import com.igorpolvora.workshopmongo.repository.PostRepository;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -12,15 +17,27 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
-        
+        postRepository.deleteAll();
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/01/2026"), "Bom dia", "Acordei feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
